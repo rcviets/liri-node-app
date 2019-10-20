972c9ece0cdec1a90d0c7ce935fc3b5aec4c0502
Randamus <rcviets@gmail.com>
2019-10-20 16:49:10 -0600

    working switch function

--- liri.js
+++ liri.js
@@ -18,13 +18,38 @@ var spotify = new Spotify(keys.spotify);
 function userInput(){
     var searchType = process.argv[2]
     var searchTerm = process.argv.slice(3).join('');
+    whichSearch(searchType, searchTerm);
 }
 
 //Switch function to determine which API to search
+function whichSearch(searchType, searchTerm) {
+    switch(searchType) {
+        case 'concert-this': {
+            searchBIT(searchTerm);
+        }
+        break;
+        case 'spotify-this-song': {
+            searchSpotify(searchTerm);
+        }
+        break;
+        case 'movie-this': {
+            searchOMDB(searchTerm);
+        }
+        break;
+        case 'do-what-it-says': {
+            searchRandom(searchTerm);
+        }
+        break;
+        default: {
+            console.log('Please make a valid request');
+        }
+    }
+}
 
 //Functions for individual API searches
     //Spotify Search
     //OMDB Search
     //Bands in Town Search
     //Random.txt Search
-    
+
+userInput();
